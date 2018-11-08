1c34052b037569308db04d448e1d1366aa4152b8
Vu Tran <vtran20@gmail.com>
2018-11-07 20:57:51 -0500

    Allow deploy app to tomcat

--- src/main/java/com/easysoft/finance/Application.java
+++ src/main/java/com/easysoft/finance/Application.java
@@ -3,6 +3,8 @@ package com.easysoft.finance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,10 +12,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EntityScan("com.easysoft.finance.domain")
 @EnableJpaRepositories("com.easysoft.finance.repository")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<Application> applicationClass = Application.class;
 }
